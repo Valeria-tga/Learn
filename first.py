b477a504e87fd7c0f509e4090733628ffb395ce3
Valeria <valeri.tga@gmail.com>
2024-08-02 19:21:50 +0200

    End theory  chapter two

---
+++ first.py
@@ -0,0 +1,12 @@
+# a,b = eval(input('Enter two number: ')) #Числа вводятся через запятую
+
+# c,d=input('Enter two number: ')
+#
+# print(a,b)
+# print(c,d)
+
+#===============================================================================
+
+text='Hello World'
+
+print(eval(text))
