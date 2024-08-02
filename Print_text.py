634c03a459b0ea83b05d9e6091bf1cb28a33041c
Valeria <valeri.tga@gmail.com>
2024-08-02 10:33:19 +0200

    B
    commit end the chapter one

--- Print_text.py
+++ Print_text.py
@@ -1,26 +1,29 @@
+# chapter 1
+
 #print("Hi again")
 
-text = 'hi'
-expr = '2+3*5'
+# text = 'hi'
+# expr = '2+3*5'
+#
+# print(type(text))
+#
+# print(type(expr)) #str type
+#
+# print(type(eval(expr))) #int type
+#
+#
+# #удаление элементов списка?
+#
+# li=[1,2,3,4]
+# li[1:-1]=[]
+# print(li)
+#
+# del li[-1]
+# print(li)
+#
+# li2=list('Hello')
+# print(li2)
+# li3=list(reversed(li2))
+# print(li3)
 
-print(type(text))
-
-print(type(expr)) #str type
-
-print(type(eval(expr))) #int type
-
-
-#удаление элементов списка?
-
-li=[1,2,3,4]
-li[1:-1]=[]
-print(li)
-
-del li[-1]
-print(li)
-
-li2=list('Hello')
-print(li2)
-li3=list(reversed(li2))
-print(li3)
 
