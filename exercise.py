e51d546394ce771a122fdb4cff6303da349d5ec7
Valeria <valeri.tga@gmail.com>
2024-08-02 19:52:03 +0200

    do two exercise

---
+++ exercise.py
@@ -0,0 +1,34 @@
+#1
+
+# number=int(input('Enter Number: '))
+
+
+def count_digits(number):
+    k=0
+    while True:
+        if number>0:
+            number=number//10
+            k=k+1
+        else:
+            return k
+
+
+# print(f'Amount of digits in a {number}: {count_digits(number)}')
+
+#2
+
+number=int(input('Enter Number: '))
+count_number=count_digits(number)
+def replace_nine(number,count_number):
+    number=str(number)
+    result=''
+    for i in number:
+        if i!=9:
+            rest=str(9-int(i))
+            result=result+rest
+        else:
+            result=result+'9'
+    return result
+
+
+print(f'{replace_nine(number,count_number)}')
