5dd0bfad19f349e6c40e89e95fb320b45a136980
Valeria <valeri.tga@gmail.com>
2024-08-04 00:20:54 +0200

    test git

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hello")
+txt=input('Введите текст: ')
+symb=input('Какую букву нужно найти?: ')
+num=txt.find(symb)
+L=[]
+while num!=-1:
+    L.append(num)
+    num=txt.find(symb,num+1)
+
+if L==0:
+    print('Такой буквы нет в тексте')
+else:
+    print(f'Позиции буквы {symb} в тексте: {L}')
